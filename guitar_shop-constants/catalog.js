36930fa9d8f34c632a775d5d87d13cd1abf6d27d
AndrOmelin <melinus1990@gmail.com>
2023-03-16 00:11:11 +0200

    created the basis of the project - Guitar Shop-15/03/23

---
+++ guitar_shop-constants/catalog.js
@@ -0,0 +1,80 @@
+const CATALOG = [
+    {
+      id: 'element-1',
+      name: 'EPIPHONE CRESTWOOD CUSTOM TREMOTONE CHERRY(1)',
+      img: 'https://i.ibb.co/NLDPLb4/EPIPHONE-CRESTWOOD-CUSTOM-TREMOTONE-CHERRY-1.jpg', 
+      // само правильно картинку заливать на хостинг и сюда↑ вставлять ссылку
+      price: 20000,
+    },
+
+    {
+        id: 'element-2',
+        name: 'EPIPHONE CRESTWOOD CUSTOM TREMOTONE POLARIS WHITE(2)',
+        img: '',
+        price: 0,
+    },
+
+    {
+        id: 'element-3',
+        name: 'FENDER AMERICAN PRO II JAZZMASTER RW MERCURY(3)',
+        img: '',
+        price: 0,
+
+    },
+
+    {
+        id: 'element-4',
+        name: 'FENDER KURT COBAIN JAG-STANG FIESTA RED(4)',
+        img: '',
+        price: 0,
+
+    },
+
+    {
+        id: 'element-5',
+        name: 'FENDER NOVENTA JAZZMASTER PF WALNUT(5)',
+        img: '',
+        price: 0,
+
+    },
+
+    {
+        id: 'element-6',
+        name: 'FENDER NOVENTA STRATOCASTER MN DAPHNE BLUE(6)',
+        img: '',
+        price: 0,
+
+    },
+
+    {
+        id: 'element-7',
+        name: 'FENDER PARALLEL UNIVERSE WHITEGUARD STRAT MN VBL(7)',
+        img: '',
+        price: 0,
+
+    },
+
+    {
+        id: 'element-8',
+        name: 'FENDER PLAYER PLUS NASHVILLE TELECASTER PF OSPK(8)',
+        img: '',
+        price: 0,
+
+    },
+
+    {
+        id: 'element-9',
+        name: 'FENDER VINTERA 60s JAGUAR PFN 3-COLOR SUNBURST(9)',
+        img: '',
+        price: 0,
+
+    },
+
+    {
+        id: 'element-10',
+        name: 'GRETSCH G2627T STREAMLINER FSR SHORELINE GOLD(10)',
+        img: '',
+        price: 0,
+
+    },
+] 
