430f2bdd056df912beef965088b780280efeaa13
AndrOmelin <melinus1990@gmail.com>
2023-03-16 01:07:51 +0200

    created array in file catalog.js-16/03/23

--- guitar_shop-constants/catalog.js
+++ guitar_shop-constants/catalog.js
@@ -4,77 +4,77 @@ const CATALOG = [
       name: 'EPIPHONE CRESTWOOD CUSTOM TREMOTONE CHERRY(1)',
       img: 'https://i.ibb.co/NLDPLb4/EPIPHONE-CRESTWOOD-CUSTOM-TREMOTONE-CHERRY-1.jpg', 
       // само правильно картинку заливать на хостинг и сюда↑ вставлять ссылку
-      price: 20000,
+      price: 100,
     },
 
     {
         id: 'element-2',
         name: 'EPIPHONE CRESTWOOD CUSTOM TREMOTONE POLARIS WHITE(2)',
-        img: '',
-        price: 0,
+        img: 'https://i.ibb.co/X3k39Q2/EPIPHONE-CRESTWOOD-CUSTOM-TREMOTONE-POLARIS-WHITE-2.jpg',
+        price: 200,
     },
 
     {
         id: 'element-3',
         name: 'FENDER AMERICAN PRO II JAZZMASTER RW MERCURY(3)',
-        img: '',
-        price: 0,
+        img: 'https://i.ibb.co/qWf56pT/FENDER-AMERICAN-PRO-II-JAZZMASTER-RW-MERCURY-3.jpg',
+        price: 300,
 
     },
 
     {
         id: 'element-4',
         name: 'FENDER KURT COBAIN JAG-STANG FIESTA RED(4)',
-        img: '',
-        price: 0,
+        img: 'https://i.ibb.co/YRC5BXy/FENDER-KURT-COBAIN-JAG-STANG-FIESTA-RED-4.jpg',
+        price: 400,
 
     },
 
     {
         id: 'element-5',
         name: 'FENDER NOVENTA JAZZMASTER PF WALNUT(5)',
-        img: '',
-        price: 0,
+        img: 'https://i.ibb.co/xH96jHd/FENDER-NOVENTA-JAZZMASTER-PF-WALNUT-5.jpg',
+        price: 500,
 
     },
 
     {
         id: 'element-6',
         name: 'FENDER NOVENTA STRATOCASTER MN DAPHNE BLUE(6)',
-        img: '',
-        price: 0,
+        img: 'https://i.ibb.co/nL6g66b/FENDER-NOVENTA-STRATOCASTER-MN-DAPHNE-BLUE-6.jpg',
+        price: 600,
 
     },
 
     {
         id: 'element-7',
         name: 'FENDER PARALLEL UNIVERSE WHITEGUARD STRAT MN VBL(7)',
-        img: '',
-        price: 0,
+        img: 'https://i.ibb.co/FHX5NXh/FENDER-PARALLEL-UNIVERSE-WHITEGUARD-STRAT-MN-VBL-7.jpg',
+        price: 700,
 
     },
 
     {
         id: 'element-8',
         name: 'FENDER PLAYER PLUS NASHVILLE TELECASTER PF OSPK(8)',
-        img: '',
-        price: 0,
+        img: 'https://i.ibb.co/LRJcyn7/FENDER-PLAYER-PLUS-NASHVILLE-TELECASTER-PF-OSPK-8.jpg',
+        price: 800,
 
     },
 
     {
         id: 'element-9',
         name: 'FENDER VINTERA 60s JAGUAR PFN 3-COLOR SUNBURST(9)',
-        img: '',
-        price: 0,
+        img: 'https://i.ibb.co/82k3gxw/FENDER-VINTERA-60s-JAGUAR-PFN-3-COLOR-SUNBURST-9.jpg',
+        price: 900,
 
     },
 
     {
         id: 'element-10',
         name: 'GRETSCH G2627T STREAMLINER FSR SHORELINE GOLD(10)',
-        img: '',
-        price: 0,
+        img: 'https://i.ibb.co/xqqWSXw/GRETSCH-G2627-T-STREAMLINER-FSR-SHORELINE-GOLD-10.jpg',
+        price: 1000,
 
     },
 ] 
